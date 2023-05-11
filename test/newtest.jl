8917e264b2a4880234e8d8e3ab756316d6673b20
chenningcong <393069484@qq.com>
2023-05-11 13:54:41 +0800

    Self check test modified

--- test/newtest.jl
+++ test/newtest.jl
@@ -1,5 +1,11 @@
 module MyTest
-include("../src/SimpleTypeChecker.jl")
+
+import SimpleTypeChecker
+
+module Wrapper
+    include("../src/SimpleTypeChecker.jl")
+end
+
 #=
 include("abstract.jl")
 ctx = SimpleTypeChecker.Inference.GlobalContext()
@@ -10,9 +16,9 @@ const path = abspath(".")
 ctx = SimpleTypeChecker.Inference.GlobalContext()
 #SimpleTypeChecker.API.addFile!(ctx, SimpleTypeChecker.SyntaxDefinition, joinpath(path, "src/adaptor/SyntaxDefinition.jl"))
 #SimpleTypeChecker.API.addFile!(ctx, SimpleTypeChecker.SyntaxAdaptor, joinpath(path, "src/adaptor/SyntaxAdaptor.jl"))
-SimpleTypeChecker.API.addFile!(ctx, SimpleTypeChecker.Inference, joinpath(path, "src/adaptor/InferenceError.jl"))
-SimpleTypeChecker.API.addFile!(ctx, SimpleTypeChecker.Inference, joinpath(path, "src/adaptor/Inference.jl"))
-SimpleTypeChecker.API.addFile!(ctx, SimpleTypeChecker.Inference, joinpath(path, "src/adaptor/JuExprAdaptor.jl"))
+SimpleTypeChecker.API.addFile!(ctx, Wrapper.SimpleTypeChecker.Inference, joinpath(path, "src/adaptor/InferenceError.jl"))
+SimpleTypeChecker.API.addFile!(ctx, Wrapper.SimpleTypeChecker.Inference, joinpath(path, "src/adaptor/Inference.jl"))
+SimpleTypeChecker.API.addFile!(ctx, Wrapper.SimpleTypeChecker.Inference, joinpath(path, "src/adaptor/JuExprAdaptor.jl"))
 #SimpleTypeChecker.API.addFile!(ctx, SimpleTypeChecker.Inference, joinpath(path, "src/adaptor/JuExprValidator.jl"))
 SimpleTypeChecker.API.runCheck!(ctx)
 

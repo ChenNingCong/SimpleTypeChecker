83514ba815113a07f69155cd0640e4cf81311a58
chenningcong <393069484@qq.com>
2023-05-19 02:51:31 +0800

    Fix API

--- src/adaptor/API.jl
+++ src/adaptor/API.jl
@@ -4,4 +4,14 @@ import ..SimpleTypeChecker.Inference.GlobalContext
 import ..SimpleTypeChecker.Inference.addFile!
 import ..SimpleTypeChecker.Inference.runCheck!
 
-export addFile!, runCheck!, GlobalContext
+function check(ctx::SimpleTypeChecker.Inference.GlobalContext, f, tt)
+    ci = Base.code_typed(f,tt)[1].first
+    if ci isa Core.CodeInfo
+        mi = ci.parent
+        push!(ctx.queue, mi)
+        runCheck!(ctx)
+    end
+    return
+end
+
+export addFile!, runCheck!, GlobalContext, @nocheck, check

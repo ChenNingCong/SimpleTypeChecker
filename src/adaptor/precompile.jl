16e50957cb76d9e96ce79d8fef73e711a0e0574b
chenningcong <393069484@qq.com>
2023-05-18 13:13:49 +0800

    Minor fix of precompilation file

--- src/adaptor/precompile.jl
+++ src/adaptor/precompile.jl
@@ -1,3 +1,3 @@
-precompile(SimpleTypeChecker.Inference.parseJuExpr, (String, String))
 precompile(SimpleTypeChecker.SyntaxAdaptor.parseJuAST, (String, String))
 precompile(SimpleTypeChecker.Inference.inferExpr, (SimpleTypeChecker.Inference.Engine, SimpleTypeChecker.Inference.Context, SimpleTypeChecker.Inference.JuAST))
+precompile(SimpleTypeChecker.Inference.runCheck!, (SimpleTypeChecker.Inference.GlobalContext,))

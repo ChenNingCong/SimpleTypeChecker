dc7b10426ffab85c5db5305108bd972d3138486f
chenningcong <393069484@qq.com>
2023-06-05 09:35:06 +0800

    add api for loadPackage

--- src/adaptor/API.jl
+++ src/adaptor/API.jl
@@ -15,4 +15,49 @@ function check(ctx::SimpleTypeChecker.Inference.GlobalContext, f, tt)
     return
 end
 
-export addFile!, runCheck!, GlobalContext, @nocheck, check
+
+
+function loadPackage(s)::GlobalContext
+    name = Symbol(String(s))
+    mod = Main
+    for i in 1:2
+        try
+            Core.eval(Main,:(using $name))
+            mod = Core.eval(Main, name)
+        catch e
+            println(e)
+        end
+    end
+    if mod == Main
+        error("Failed to include module")
+    end
+    path = dirname(abspath(pathof(mod)))
+    files = String[]
+    for (root, dir, files_) in collect(walkdir(path))
+        for f in files_
+            apath = joinpath(path, root, f)
+            println("Scanning file from package $mod", apath)
+            push!(files, apath)
+        end
+    end
+    ctx = GlobalContext()
+    for i in files
+        full = joinpath(path, i)
+        println(mod, " ", full)
+        addFile!(ctx, mod, full)
+    end
+    return ctx
+end
+
+function checkFactory(ctx::GlobalContext)
+    function helper(x...)
+        check(ctx, x...)
+    end
+    return helper
+end
+
+macro code_checked(ctx, ex0...)
+    InteractiveUtils.gen_call_with_extracted_types_and_kwargs(__module__, :(checkFactory($ctx)), ex0)
+end
+
+export addFile!, runCheck!, GlobalContext, @nocheck, check, loadPackage
